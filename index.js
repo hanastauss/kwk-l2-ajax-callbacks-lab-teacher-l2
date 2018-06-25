c9ca5443bd5df258d849df6618e1b65ecdaf49ee
Hana Stauss <hanastauss@icloud.com>
2018-06-25 20:42:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ $('#searchButton').click(searchRepositories() )
 function searchRepositories(){
   let term = $('#searchTerms').val()
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
-      $('#results').html(renderSearchResults(data))
+      $('#results').html(renderSearchResults(data)) }) 
     
 }
   
